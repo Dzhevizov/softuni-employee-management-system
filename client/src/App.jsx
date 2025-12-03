3de4e68845c69f3966fff3db7f428264b7af70c9
Hristo Dzhevizov <dzhevizov@abv.bg>
2025-12-04 01:53:09 +0200

    Add base layout components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,27 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Pagination from "./components/Pagination"
+import SearchForm from "./components/SearchForm"
+import UserList from "./components/UserList"
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Header />
+
+      <main className="main">
+        <section className="card users-container">
+          <SearchForm />
+
+          <UserList />
+
+          <button className="btn-add btn">Add new user</button>
+
+          <Pagination />
+        </section>
+      </main>
+
+      <Footer />
     </>
   )
 }
